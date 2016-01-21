15986257a1072081576307b57eb87857eb123e47
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-21 20:41:41 +0000

    Add new and create action and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,25 @@ class PostsController < ApplicationController
   def index 
     @posts = Post.all
   end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "New Post Added"
+      redirect_to @post
+    else
+      flash[:error] = "There was an error"
+      render :new
+    end
+  end
+  
+  private
+  
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
