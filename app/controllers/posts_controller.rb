7d8c867bdbff644fb5bb56efdfbe1f5bc7c46848
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-21 21:02:41 +0000

    Add edit and update actions, and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,21 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
   
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:success] = "Update Succesful"
+      redirect_to @post
+    else
+      flash[:error] = "There was an error"
+      render :edit
+    end
+  end
+  
   private
   
     def post_params
