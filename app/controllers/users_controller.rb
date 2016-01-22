e4868680a6f4182a78fb1995baa0ddea7c475ee7
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-22 18:59:11 +0000

    Add create action to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,20 @@ class UsersController < ApplicationController
   end
   
   def create
+    user = User.new(user_params)
+    if user.save
+      flash[:success] = "You have signed up!"
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      flash[:error] = "Sorry there was an error."
+      redirect_to '/signup'
+    end
   end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
