1e63d2c00cc135e75c84ff34514ccaf26fcf709b
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-22 09:57:24 +0000

    Add create action to CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  
+  def create 
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    
+    if @comment.save
+      flash[:success] = "New comment added."
+      redirect_to @post
+    else  
+      flash[:error] = "There was an error!"
+      redirect_to @post
+    end
+  end
+  
+  private
+    
+    def comment_params
+      params.require(:comment).permit(:title, :body)
+    end
 end
