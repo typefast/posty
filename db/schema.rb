89906c3d5511e64c585dd9286611306d85cdc788
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-23 11:10:50 +0000

    Add Role to Users rable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122193400) do
+ActiveRecord::Schema.define(version: 20160123110829) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160122193400) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
