b9b25551fa9dccac7a7d89b434804959b89f4f0e
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-23 11:31:35 +0000

    Add admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+  
+    a = User.create(name: "admin1", email: "admin1@example.com", password: "admin1234", role: "admin")
